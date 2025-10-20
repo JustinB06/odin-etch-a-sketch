78019e376234467ac1d0d055a4cf0b4793d12e4e
JustinB06 <practice@test.com>
2025-10-19 21:02:37 -0400

    Decouple styling and DOM object creation
    Created styles.css. Removed the inline styling
    from scripts.js and moved it into styles.css,
    into a rule that selects class "square".
    
    Now, to add the necessary styling to a created
    DOM object, we just add the class "square" to its
    class list.

--- scripts.js
+++ scripts.js
@@ -12,19 +12,19 @@ for (let i = 0; i < 16; i++) {
   /* Create each element of a row */
   for (let j = 0; j < 16; j++) {
     let squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    rowContainerDiv.appendChild(squareDiv);
 
     /* Will cause the flex item to grow and
     shrink, from its baseline CSS width */
-    squareDiv.style["flex"] = "1 1 auto";
+    // squareDiv.style["flex"] = "1 1 auto";
 
     /* Will set the element's dimensions to a 
     width-to-height ratio of 1, forming a square. */
-    squareDiv.style["aspect-ratio"] = "1 / 1";
-    squareDiv.style["width"] = "1rem";
+    // squareDiv.style["aspect-ratio"] = "1 / 1";
+    // squareDiv.style["width"] = "1rem";
 
-    squareDiv.style["border"] = "0.1rem solid black";
-
-    rowContainerDiv.appendChild(squareDiv);
+    // squareDiv.style["border"] = "0.1rem solid black";
   }
 
   containerRef.appendChild(rowContainerDiv);
