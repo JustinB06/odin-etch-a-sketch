25d5df91f88770c90236db7f451d6cdace591f49
JustinB06 <practice@test.com>
2025-10-19 20:46:02 -0400

    Create a 16x16 grid on index.html at runtime
    Using scripts.js, a 16x16 grid of squares was
    created through DOM manipulation.
    
    "flex: 1 1 auto" allows the squares to grow and
    shrink to take up all available space and
    "aspect-ratio: 1 / 1" ensures that the squares
    remain square shaped. Thus, the squares do not
    need an inherent CSS "width".
    
    I have placed each square div inside of a row div
    container, which then goes inside an overall div
    container. I believe that this will allow each row
    of squares to grow and shrink without issue, but I
    will have to see if flex-wrap can create the same
    layout. If so, the "middleman" row divs can be
    removed.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
+/* Get DOM object references */
+let containerRef = document.querySelector("#container");
 
+/* Create a 16x16 grid of square divs */
+for (let i = 0; i < 16; i++) {
+  /* I think we put every row of divs into 
+    its own flexbox container, which will allow
+    them to expand horizontally properly */
+  let rowContainerDiv = document.createElement("div");
+  rowContainerDiv.style["display"] = "flex";
+
+  /* Create each element of a row */
+  for (let j = 0; j < 16; j++) {
+    let squareDiv = document.createElement("div");
+
+    /* Will cause the flex item to grow and
+    shrink, from its baseline CSS width */
+    squareDiv.style["flex"] = "1 1 auto";
+
+    /* Will set the element's dimensions to a 
+    width-to-height ratio of 1, forming a square. */
+    squareDiv.style["aspect-ratio"] = "1 / 1";
+    squareDiv.style["width"] = "1rem";
+
+    squareDiv.style["border"] = "0.1rem solid black";
+
+    rowContainerDiv.appendChild(squareDiv);
+  }
+
+  containerRef.appendChild(rowContainerDiv);
+}
